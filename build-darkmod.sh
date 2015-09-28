d75cf69002e0c7f7fe23b977b9e74e59f38876b5
Simon Lees <simon@simotek.net>
2015-09-28 20:06:39 +0930

    use regexp to pull out html color, hopefully more stable

--- build-darkmod.sh
+++ build-darkmod.sh
@@ -67,15 +67,20 @@ TMP_EXTRACTED=${TMP_MID#${TMP_MID:0:46}}
 #form the html number
 HIGH_HTML="#${TMP_EXTRACTED:0:6}"
 #form the rgb number
+
+HIGH_HTML=$(convert enlightenment-elementary/img-color-convd/bg_glow_in.png -crop "1x1+0+0" txt:- | awk 'match($0, /#[0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f][0-9A-Fa-f]/) {print substr($0, RSTART, RLENGTH)}'
+)
 TMP_RGB=${TMP_EXTRACTED#${TMP_EXTRACTED:0:14}}
 TMP_RGB2=${TMP_RGB%")"}
 TMP_RGB3=${TMP_RGB2//,/ }
 HIGH_RGB=$(echo "$TMP_RGB3"| rev | cut -c 2- | rev)
- 
+
 set $HIGH_RGB
 HIGH_RED=$1
 HIGH_GREEN=$2
 HIGH_BLUE=$3
+
+echo "Colors are $HIGH_HTML, $HIGH_RGB"
 #if we don't have a valid color error
 if [ -z "$HIGH_HTML" ]; then
     error "Highlight Color could not be determined"
@@ -122,7 +127,7 @@ report_on_error cp -a colorclasses.edc colorclasses-dm.edc
 report_on_error cp -a fonts.edc fonts-dm.edc
 report_on_error cp -a macros.edc macros-dm.edc
 
-# Replace background and highlights in edc 
+# Replace background and highlights in edc
 for F in `find edc-dm colorclasses-dm.edc macros-dm.edc -iname "*.edc"`; do
     # Highlight color
     if [[ "$HIGH_RGB" != "51 153 255" ]]; then
@@ -130,47 +135,47 @@ for F in `find edc-dm colorclasses-dm.edc macros-dm.edc -iname "*.edc"`; do
         sed -i "s/#3399ff/$HIGH_HTML/g" $F
         # for battery
         sed -i "s/r = 51, g = 153, b = 255/r = $HIGH_RED, g = $HIGH_GREEN, b = $HIGH_BLUE/g" $F
-    fi 
-    
+    fi
+
     # File manager background
     if [[ "$FILEMGR_BKND_RGB" != "64 64 64" ]]; then
         sed -i "s/64 64 64/$FILEMGR_BKND_RGB/g" $F
         sed -i "s/#404040/$FILEMGR_BKND_HTML/g" $F
     fi
-    
+
     # file manager alt
     if [[ "$FILEMGR_ALT_BKND_RGB" != "56 56 56" ]]; then
         sed -i "s/56 56 56/$FILEMGR_ALT_BKND_RGB/g" $F
         sed -i "s/#383838/$FILEMGR_ALT_BKND_HTML/g" $F
     fi
-    
+
     # File manager image background
     if [[ "$FILEMGR_IMG_BKND_RGB" != "48 48 48" ]]; then
         sed -i "s/48 48 48/$FILEMGR_IMG_BKND_RGB/g" $F
         sed -i "s/#303030/$FILEMGR_IMG_BKND_HTML/g" $F
     fi
-    
+
     # Grey boxes in pager
     if [[ "$FILEMGR_MID_GREY_RGB" != "50 50 50" ]]; then
         sed -i "s/50 50 50/$FILEMGR_MID_GREY_RGB/g" $F
         sed -i "s/#323232/$FILEMGR_MID_GREY_HTML/g" $F
     fi
-    
+
     # modify html versions of text for textblock
     if [[ "$FNT_DEFAULT_HTML" != "#FFFFFF" ]]; then
         sed -i "s/#ffffff/$FNT_DEFAULT_HTML/gI" $F
         sed -i "s/#ffff/$FNT_DEFAULT_HTML/gI" $F
     fi
-    
+
     if [[ "$FNT_DEFAULT_SHADOW_HTML" != "#00000080" ]]; then
         sed -i "s/#00000080/$FNT_DEFAULT_SHADOW_HTML/gI" $F
     fi
-    
+
     # Disabled text
     if [[ "$FNT_DISABLED_HTML" != "#151515" ]]; then
         sed -i "s/#151515/$FNT_DISABLED_HTML/g" $F
     fi
-    
+
     if [[ "$FNT_DISABLED_SHADOW_HTML" != "#FFFFFF19" ]]; then
         sed -i "s/#FFFFFF19/$FNT_DISABLED_SHADOW_HTML/gI" $F
     fi
@@ -184,35 +189,35 @@ for F in `find fonts-dm.edc -iname "*.edc"`; do
         sed -i "s/#ffffff/$FNT_DEFAULT_HTML/gI" $F
         sed -i "s/#ffff/$FNT_DEFAULT_HTML/gI" $F
     fi
-    
+
     if [[ "$FNT_DEFAULT_SHADOW_RGB" != "0 0 0 128" ]]; then
         sed -i "s/0 0 0 128/$FNT_DEFAULT_SHADOW_RGB/g" $F
         sed -i "s/#00000080/$FNT_DEFAULT_SHADOW_HTML/gI" $F
     fi
-    
+
     # Highlight color
     if [[ "$HIGH_RGB" != "51 153 255" ]]; then
         sed -i "s/51 153 255/$HIGH_RGB/g" $F
         sed -i "s/#3399ff/$HIGH_HTML/g" $F
     fi
-    
+
     # Disabled text
     if [[ "$FNT_DISABLED_HTM" != "#151515" ]]; then
         sed -i "s/21 21 21/$FNT_DISABLED_RGB/g" $F
         sed -i "s/16 16 16 16/16 $FNT_DISABLED_RGB/g" $F
         sed -i "s/#151515/$FNT_DISABLED_HTML/g" $F
     fi
-    
+
     if [[ "$FNT_DISABLED_SHADOW_RG" != "255 255 255 25" ]]; then
         sed -i "s/255 255 255 25\ns/$FNT_DISABLED_SHADOW_RGB\n/g " $F
         sed -i "s/#FFFFFF19/$FNT_DISABLED_SHADOW_HTML/gI" $F
     fi
-    
+
     # Various Grey text need 4 colors so it doesn't overwrite the name instead
     sed -i "s/192 192 192 192/192 $FNT_GREY_192_RGB/g" $F
     sed -i "s/172 172 172 172/172 $FNT_GREY_172_RGB/g" $F
     sed -i "s/152 152 152 152/152 $FNT_GREY_152_RGB/g" $F
-    
+
 done
 
 sed -i "s/Dark/$THEME_NAME/g" edc-dm/about-theme.edc
@@ -242,22 +247,22 @@ popd
 if [ -n "$TERMINOLOGY_THEME_PATH" ]; then
     mkdir $TERMINOLOGY_THEME_PATH/img-bak
     report_on_error cp -vr $TERMINOLOGY_THEME_PATH/images/* $TERMINOLOGY_THEME_PATH/img-bak
-    
+
     moveAllTerminologyHighlightImages
     moveAllTerminologyBackgroundImages
     moveAllTerminologyShadowImages
-    
+
     mv $TERMINOLOGY_THEME_PATH/images $TERMINOLOGY_THEME_PATH/img-no-change
     success "    Finished moving terminology images"
 
     mkdir $TERMINOLOGY_THEME_PATH/img-color-convd
-    
+
     pushd $TERMINOLOGY_THEME_PATH/img-color
     for F in `find -iname "*.png"`; do
             convert $F -modulate $HIGH_BRIGHTNESS,$HIGH_SATURATION,$HIGH_HUE ../img-color-convd/$F
     done
     popd
-    
+
     if [ -d "$TERMINOLOGY_THEME_PATH/img-color-manual" ]; then
         pushd $TERMINOLOGY_THEME_PATH/img-color-manual
             for F in `find -iname "*.png"`; do
@@ -265,7 +270,7 @@ if [ -n "$TERMINOLOGY_THEME_PATH" ]; then
             done
         popd
     fi
-    
+
     # Converting background images
     pushd $TERMINOLOGY_THEME_PATH/img-bgnd
     for F in `find -iname "*.png"`; do
@@ -280,14 +285,14 @@ if [ -n "$TERMINOLOGY_THEME_PATH" ]; then
     done
     popd
 
-    
+
     pushd $TERMINOLOGY_THEME_PATH
     report_on_error cp -a default.edc default-dm.edc
     report_on_error cp -a default_colors.in.edc default-dm_colors.in.edc
-    
+
     sed -i "s/default_colors.in.edc/default-dm_colors.in.edc/g" default-dm.edc
-    
-    # Replace background and highlights in edc 
+
+    # Replace background and highlights in edc
     for F in `find default-dm.edc default-dm_colors.in.edc -iname "*.edc"`; do
         # Highlight color
         if [[ "$HIGH_RGB" != "51 153 255" ]]; then
@@ -295,51 +300,51 @@ if [ -n "$TERMINOLOGY_THEME_PATH" ]; then
             sed -i "s/#3399ff/$HIGH_HTML/g" $F
             sed -i "s/r = 51, g = 153, b = 255/r = $HIGH_RED, g = $HIGH_GREEN, b = $HIGH_BLUE/g" $F
         fi
-        
-        
+
+
         # File manager background
         if [[ "$FILEMGR_BKND_RGB" != "64 64 64" ]]; then
             sed -i "s/64 64 64/$FILEMGR_BKND_RGB/g" $F
             sed -i "s/#404040/$FILEMGR_BKND_HTML/g" $F
         fi
-        
+
         # file manager alt
         if [[ "$FILEMGR_ALT_BKND_RGB" != "56 56 56" ]]; then
             sed -i "s/56 56 56/$FILEMGR_ALT_BKND_RGB/g" $F
             sed -i "s/#383838/$FILEMGR_ALT_BKND_HTML/g" $F
         fi
-        
+
         # File manager image background
         if [[ "$FILEMGR_IMG_BKND_RGB" != "48 48 48" ]]; then
             sed -i "s/48 48 48/$FILEMGR_IMG_BKND_RGB/g" $F
             sed -i "s/#303030/$FILEMGR_IMG_BKND_HTML/g" $F
         fi
-        
+
         # Grey boxes in pager
         if [[ "$FILEMGR_MID_GREY_RGB" != "50 50 50" ]]; then
             sed -i "s/50 50 50/$FILEMGR_MID_GREY_RGB/g" $F
             sed -i "s/#323232/$FILEMGR_MID_GREY_HTML/g" $F
         fi
-        
+
         # modify html versions of text for textblock
         if [[ "$FNT_DEFAULT_HTML" != "#ffffff" ]]; then
             sed -i "s/#ffffff/$FNT_DEFAULT_HTML/gI" $F
             sed -i "s/#ffff/$FNT_DEFAULT_HTML/gI" $F
         fi
-        
+
         if [[ "$FNT_DEFAULT_SHADOW_HTML" != "#00000080" ]]; then
             sed -i "s/#00000080/$FNT_DEFAULT_SHADOW_HTML/gI" $F
         fi
-        
+
         # Disabled text
         if [[ "$FNT_DISABLED_HTML" != "#151515" ]]; then
             sed -i "s/#151515/$FNT_DISABLED_HTML/g" $F
         fi
-        
+
         if [[ "$FNT_DISABLED_SHADOW_HTML" != "#FFFFFF19" ]]; then
             sed -i "s/#FFFFFF19/$FNT_DISABLED_SHADOW_HTML/gI" $F
         fi
-        
+
         #terminology background
         if [[ "$TERMINOLOGY_BACKGROUND" != "32 32 32" ]]; then
             sed -i "s/32 32 32/$TERMINOLOGY_BACKGROUND/g" $F
