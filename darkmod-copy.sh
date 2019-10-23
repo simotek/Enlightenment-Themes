f57047259f3d60c9e1d69697a538e3b80003cfbe
Simon Lees <sflees@suse.de>
2019-10-23 17:15:13 +1030

    darkmod-copy -> sort images

--- darkmod-copy.sh
+++ darkmod-copy.sh
@@ -40,7 +40,7 @@ moveShadowImage(){
     # if file is in img-manual don't copy
     if [ ! -f $ELM_ENLIGHT_THEME_PATH/$MANUAL_IMAGE_DIR/$1 ]; then
         # check if its a manual image
-        echo "cvd: $MANUAL_IMAGE_CONVD_DIR" 
+        echo "cvd: $MANUAL_IMAGE_CONVD_DIR"
         echo "$ELM_ENLIGHT_THEME_PATH/$MANUAL_IMAGE_CONVD_DIR/$1"
         if [ -f $ELM_ENLIGHT_THEME_PATH/$MANUAL_IMAGE_CONVD_DIR/$1 ]; then
             echo "Shadow $1"
@@ -54,120 +54,24 @@ moveShadowImage(){
 moveAllHighlightImages(){
     mkdir $ELM_ENLIGHT_THEME_PATH/img-color
 
-    moveHighlightImage glow_lock_locked.png
-    moveHighlightImage icon_border_close.png
-    moveHighlightImage diagonal_stripes.png
-    moveHighlightImage mini_blue_glow_arrow_1.png
-    moveHighlightImage glow_exclam.png
-    moveHighlightImage logo_blue_bottom.png
-    moveHighlightImage icon_system-lock-screen.png
-    moveHighlightImage wifi_sig_3.png
-    moveHighlightImage split_h_glow.png
-    moveHighlightImage cell_sig_1.png
-    moveHighlightImage icon_system.png
-    moveHighlightImage icon_border_stack_top.png
-    moveHighlightImage col_sel_end_left.png
-    moveHighlightImage home_glow.png
-    moveHighlightImage cell_sig_3.png
-    moveHighlightImage bulb_glow.png
-    moveHighlightImage icon_border_pager.png
-    moveHighlightImage logo_blue_small.png
-    moveHighlightImage horizontal_separated_bar_glow.png
-    moveHighlightImage win_glow.png
-    moveHighlightImage icon_system-shutdown.png
-    moveHighlightImage sym_close_dark_selected.png
-    moveHighlightImage icon_border_pin.png
-    moveHighlightImage mini_blue_glow_arrow_2.png
-    moveHighlightImage icon_border_move.png
-    moveHighlightImage kbd_glow.png
-    moveHighlightImage icon_border_stack_bot.png
-    moveHighlightImage icon_border_maximize.png
-    moveHighlightImage cell_sig_4.png
-    moveHighlightImage icon_border_sendto.png
-    moveHighlightImage white_bar_vert_glow.png
-    moveHighlightImage sym_icon_op_move.png
-    moveHighlightImage outline_glow.png
-    moveHighlightImage sym_down_dark_selected.png
-    moveHighlightImage bt_sig_2.png
-    moveHighlightImage downlight_glow_left.png
-    moveHighlightImage bt_sig_1.png
-    moveHighlightImage ball_small_glow.png
-    moveHighlightImage wifi_sig_1.png
-    moveHighlightImage icon_preferences-desktop-signal-bindings.png
-    moveHighlightImage mini_blue_glow_arrow_3.png
-    moveHighlightImage icon_border_shaded.png
-    moveHighlightImage icon_border_border.png
-    moveHighlightImage downlight_glow_up.png
-    moveHighlightImage icon_border_lock.png
-    moveHighlightImage therm_content.png
-    moveHighlightImage sym_up_dark_selected.png
-    moveHighlightImage icon_enlightenment.png
-    moveHighlightImage downlight_glow.png
-    moveHighlightImage sym_reload_glow_normal.png
-    moveHighlightImage col_sel_end_bottom.png
-    moveHighlightImage mini_blue_glow_arrow_0.png
-    moveHighlightImage icon_preferences-applications-personal.png
-    moveHighlightImage icon_preferences-variables.png
-    moveHighlightImage vertical_separated_bar_glow.png
-    moveHighlightImage horiz_glow_run.png
-    moveHighlightImage icon_border_minimize.png
-    moveHighlightImage media_busy_progress.png
-    moveHighlightImage split_v_glow.png
-    moveHighlightImage bt_base.png
-    moveHighlightImage sym_up_light_selected.png
-    moveHighlightImage mini_box_glow.png
-    moveHighlightImage icon_preferences-interaction.png
-    moveHighlightImage col_sel_end_right.png
-    moveHighlightImage ic_win_move.png
-    moveHighlightImage wifi_sig_2.png
-    moveHighlightImage sym_icon_op_copy.png
-    moveHighlightImage spanner_glow.png
-    moveHighlightImage sym_heart_glow_normal.png
-    moveHighlightImage sym_down_glow_normal.png
-    moveHighlightImage icon_border_more.png
-    moveHighlightImage icon_border_remember.png
-    moveHighlightImage downlight_glow_right.png
-    moveHighlightImage sym_down_light_selected.png
-    moveHighlightImage cell_base.png
-    moveHighlightImage icon_preferences-desktop-environments.png
-    moveHighlightImage sym_right_glow_normal.png
-    moveHighlightImage icon_border_stack_norm.png
-    moveHighlightImage glow_round_corners_small.png
-    moveHighlightImage holes_tiny_glow_vert.png
-    moveHighlightImage horizontal_separated_bar_small_glow.png
-    moveHighlightImage icon_border_skip.png
-    moveHighlightImage icon_border_properties.png
-    moveHighlightImage ic_win_resize.png
-    moveHighlightImage sym_up_glow_normal.png
-    moveHighlightImage holes_tiny_glow_horiz.png
-    moveHighlightImage icon_system-suspend-hibernate.png
-    moveHighlightImage split_none_glow.png
-    moveHighlightImage icon_border_resize.png
-    moveHighlightImage ball_small_glow_intense.png
-    moveHighlightImage glow_lock_unlocked.png
-    moveHighlightImage icon_system-suspend.png
-    moveHighlightImage wifi_base.png
-    moveHighlightImage icon_system-log-out.png
-    moveHighlightImage sym_left_glow_normal.png
-    moveHighlightImage icon_border_kill.png
-    moveHighlightImage vert_glow_run.png
-    moveHighlightImage glow_lock_double.png
-    moveHighlightImage col_sel_end_top.png
-    moveHighlightImage glow_small.png
-    moveHighlightImage sym_icon_op_ask.png
     moveHighlightImage add_glow_small.png
-    moveHighlightImage logo_blue_small_glow.png
-    moveHighlightImage pointer_glow.png
-    moveHighlightImage sym_close_light_selected.png
-    moveHighlightImage tacho_base.png
-    moveHighlightImage glow_round_corners.png
-    moveHighlightImage cell_sig_2.png
-    moveHighlightImage icon_system-restart.png
-    moveHighlightImage led_light.png
-    moveHighlightImage runner_glow_horiz.png
-    moveHighlightImage runner_glow_vert.png
+    moveHighlightImage ball_small_glow_intense.png
+    moveHighlightImage ball_small_glow.png
     moveHighlightImage bg_glow_in.png
     moveHighlightImage box_glow.png
+    moveHighlightImage bt_base.png
+    moveHighlightImage bt_sig_1.png
+    moveHighlightImage bt_sig_2.png
+    moveHighlightImage bulb_glow.png
+    moveHighlightImage cell_base.png
+    moveHighlightImage cell_sig_1.png
+    moveHighlightImage cell_sig_2.png
+    moveHighlightImage cell_sig_3.png
+    moveHighlightImage cell_sig_4.png
+    moveHighlightImage col_sel_end_bottom.png
+    moveHighlightImage col_sel_end_left.png
+    moveHighlightImage col_sel_end_right.png
+    moveHighlightImage col_sel_end_top.png
     moveHighlightImage day_left_press.png
     moveHighlightImage day_left_selected.png
     moveHighlightImage day_middle_press.png
@@ -176,6 +80,13 @@ moveAllHighlightImages(){
     moveHighlightImage day_right_selected.png
     moveHighlightImage day_single_press.png
     moveHighlightImage day_single_selected.png
+    moveHighlightImage diagonal_stripes.png
+    moveHighlightImage downlight_glow_left.png
+    moveHighlightImage downlight_glow.png
+    moveHighlightImage downlight_glow_right.png
+    moveHighlightImage downlight_glow_up.png
+    moveHighlightImage efm_generic_icon_over_empty.png
+    moveHighlightImage efm_generic_icon_over.png
     moveHighlightImage emo-angry.png
     moveHighlightImage emo-angry-shout.png
     moveHighlightImage emo-crazy-laugh.png
@@ -216,13 +127,24 @@ moveAllHighlightImages(){
     moveHighlightImage emo-wink.png
     moveHighlightImage emo-worried.png
     moveHighlightImage emo-wtf.png
-    moveHighlightImage efm_generic_icon_over.png
-    moveHighlightImage efm_generic_icon_over_empty.png
     moveHighlightImage eth.png
+    moveHighlightImage glow_exclam.png
+    moveHighlightImage glow_lock_double.png
+    moveHighlightImage glow_lock_locked.png
+    moveHighlightImage glow_lock_unlocked.png
+    moveHighlightImage glow_round_corners.png
+    moveHighlightImage glow_round_corners_small.png
+    moveHighlightImage glow_small.png
     moveHighlightImage handle_pick_up_left.png
     moveHighlightImage handle_pick_up_right.png
+    moveHighlightImage holes_tiny_glow_horiz.png
+    moveHighlightImage holes_tiny_glow_vert.png
+    moveHighlightImage home_glow.png
     moveHighlightImage horiz_glow_run_big.png
+    moveHighlightImage horiz_glow_run.png
     moveHighlightImage horiz_glow_run_rev.png
+    moveHighlightImage horizontal_separated_bar_glow.png
+    moveHighlightImage horizontal_separated_bar_small_glow.png
     moveHighlightImage icon_apps.png
     moveHighlightImage icon_arrow_down_left.png
     moveHighlightImage icon_arrow_down.png
@@ -232,12 +154,32 @@ moveAllHighlightImages(){
     moveHighlightImage icon_arrow_up_left.png
     moveHighlightImage icon_arrow_up.png
     moveHighlightImage icon_arrow_up_right.png
+    moveHighlightImage icon_border_border.png
+    moveHighlightImage icon_border_close.png
+    moveHighlightImage icon_border_kill.png
+    moveHighlightImage icon_border_lock.png
+    moveHighlightImage icon_border_maximize.png
+    moveHighlightImage icon_border_minimize.png
+    moveHighlightImage icon_border_more.png
+    moveHighlightImage icon_border_move.png
+    moveHighlightImage icon_border_pager.png
+    moveHighlightImage icon_border_pin.png
+    moveHighlightImage icon_border_properties.png
+    moveHighlightImage icon_border_remember.png
+    moveHighlightImage icon_border_resize.png
+    moveHighlightImage icon_border_sendto.png
+    moveHighlightImage icon_border_shaded.png
+    moveHighlightImage icon_border_skip.png
+    moveHighlightImage icon_border_stack_bot.png
+    moveHighlightImage icon_border_stack_norm.png
+    moveHighlightImage icon_border_stack_top.png
     moveHighlightImage icon_chat.png
     moveHighlightImage icon_clock.png
     moveHighlightImage icon_close.png
     moveHighlightImage icon_delete.png
     moveHighlightImage icon_edit.png
     moveHighlightImage icon_eject.png
+    moveHighlightImage icon_enlightenment.png
     moveHighlightImage icon_file.png
     moveHighlightImage icon_folder.png
     moveHighlightImage icon_forward.png
@@ -248,80 +190,93 @@ moveAllHighlightImages(){
     moveHighlightImage icon_next.png
     moveHighlightImage icon_pause.png
     moveHighlightImage icon_play.png
+    moveHighlightImage icon_preferences-applications-personal.png
+    moveHighlightImage icon_preferences-desktop-environments.png
+    moveHighlightImage icon_preferences-desktop-signal-bindings.png
+    moveHighlightImage icon_preferences-interaction.png
+    moveHighlightImage icon_preferences-variables.png
     moveHighlightImage icon_prev.png
     moveHighlightImage icon_refresh.png
     moveHighlightImage icon_rewind.png
     moveHighlightImage icon_spanner.png
-    moveHighlightImage icon_splat.png
     moveHighlightImage icon_splat_half.png
+    moveHighlightImage icon_splat.png
     moveHighlightImage icon_stop.png
+    moveHighlightImage icon_system-lock-screen.png
+    moveHighlightImage icon_system-log-out.png
+    moveHighlightImage icon_system.png
+    moveHighlightImage icon_system-restart.png
+    moveHighlightImage icon_system-shutdown.png
+    moveHighlightImage icon_system-suspend-hibernate.png
+    moveHighlightImage icon_system-suspend.png
     moveHighlightImage icon_volume.png
     moveHighlightImage icon_warning.png
+    moveHighlightImage ic_win_move.png
+    moveHighlightImage ic_win_resize.png
     moveHighlightImage inset_bar_horiz_glow_base.png
     moveHighlightImage inset_bar_horiz_glow_inv_base.png
     moveHighlightImage inset_bar_horiz_glow_mid_base.png
     moveHighlightImage inset_bar_vert_glow_base.png
     moveHighlightImage inset_bar_vert_glow_inv_base.png
     moveHighlightImage inset_bar_vert_glow_mid_base.png
+    moveHighlightImage kbd_glow.png
+    moveHighlightImage led_light.png
     moveHighlightImage led_tiny_blue.png
-    moveHighlightImage ring_white_blue_glow.png
+    moveHighlightImage logo_blue_bottom.png
+    moveHighlightImage logo_blue_small_glow.png
+    moveHighlightImage logo_blue_small.png
+    moveHighlightImage media_busy_progress.png
+    moveHighlightImage mini_blue_glow_arrow_0.png
+    moveHighlightImage mini_blue_glow_arrow_1.png
+    moveHighlightImage mini_blue_glow_arrow_2.png
+    moveHighlightImage mini_blue_glow_arrow_3.png
+    moveHighlightImage mini_box_glow.png
+    moveHighlightImage outline_glow.png
     moveHighlightImage packagekit_base.png
+    moveHighlightImage pointer_glow.png
+    moveHighlightImage ring_white_blue_glow.png
+    moveHighlightImage runner_glow_horiz.png
+    moveHighlightImage runner_glow_vert.png
+    moveHighlightImage spanner_glow.png
+    moveHighlightImage split_h_glow.png
+    moveHighlightImage split_none_glow.png
+    moveHighlightImage split_v_glow.png
+    moveHighlightImage sym_close_dark_selected.png
+    moveHighlightImage sym_close_light_selected.png
+    moveHighlightImage sym_down_dark_selected.png
+    moveHighlightImage sym_down_glow_normal.png
+    moveHighlightImage sym_down_light_selected.png
+    moveHighlightImage sym_heart_glow_normal.png
+    moveHighlightImage sym_icon_op_ask.png
+    moveHighlightImage sym_icon_op_copy.png
+    moveHighlightImage sym_icon_op_move.png
+    moveHighlightImage sym_left_glow_normal.png
+    moveHighlightImage sym_reload_glow_normal.png
+    moveHighlightImage sym_right_glow_normal.png
+    moveHighlightImage sym_up_dark_selected.png
+    moveHighlightImage sym_up_glow_normal.png
+    moveHighlightImage sym_up_light_selected.png
+    moveHighlightImage tacho_base.png
+    moveHighlightImage therm_content.png
+    moveHighlightImage vert_glow_run.png
     moveHighlightImage vert_glow_run_rev.png
+    moveHighlightImage vertical_separated_bar_glow.png
+    moveHighlightImage white_bar_vert_glow.png
+    moveHighlightImage wifi_base.png
+    moveHighlightImage wifi_sig_1.png
+    moveHighlightImage wifi_sig_2.png
+    moveHighlightImage wifi_sig_3.png
+    moveHighlightImage win_glow.png
 }
 
 moveAllBackgroundImages(){
 # move background images
     mkdir $ELM_ENLIGHT_THEME_PATH/img-bgnd
-    moveBackgroundImage darken_square.png
-    moveBackgroundImage home_inset.png
-    moveBackgroundImage bevel_out.png
-    moveBackgroundImage slider_run_base_light_vert.png
-    moveBackgroundImage holes_vert.png
-    moveBackgroundImage runner_vert.png
-    moveBackgroundImage split_v_hilight.png
-    moveBackgroundImage hole_pixel.png
-    moveBackgroundImage vgrad_med_dark.png
-    moveBackgroundImage mini_box_bevel_shadow.png
-    moveBackgroundImage vgrad_med_darker.png
-    moveBackgroundImage holes_tiny_vert.png
-    moveBackgroundImage vgrad_tall.png
-    moveBackgroundImage inset_round_shadow.png
-    moveBackgroundImage slider_run_bevel_vert.png
-    moveBackgroundImage led_square_shading.png
-    moveBackgroundImage spanner_hilight.png
-    moveBackgroundImage holes_horiz.png
     moveBackgroundImage bevel_dark_out.png
-    moveBackgroundImage runner_horiz.png
-    moveBackgroundImage spanner_inset.png
-    moveBackgroundImage split_none_hilight.png
-    moveBackgroundImage split_v_inset.png
-    moveBackgroundImage hole_tiny.png
-    moveBackgroundImage bg_radgrad.png
-    moveBackgroundImage slider_run_base_vert.png
-    moveBackgroundImage vgrad_med_curved.png
-    moveBackgroundImage button_clicked.png
-    moveBackgroundImage slot_horiz_bottom.png
     moveBackgroundImage bevel_horiz_out.png
-    moveBackgroundImage slider_run_base_light_horiz.png
-    moveBackgroundImage slider_run_bevel_horiz.png
-    moveBackgroundImage button_normal.png
-    moveBackgroundImage split_h_inset.png
     moveBackgroundImage bevel_in.png
-    moveBackgroundImage kbd_inset.png
-    moveBackgroundImage separator_horiz.png
-    moveBackgroundImage kbd_hilight.png
-    moveBackgroundImage split_h_hilight.png
-    moveBackgroundImage inset_round_shading.png
-    moveBackgroundImage vgrad_med_lighter.png
-    moveBackgroundImage vgrad_med.png
-    moveBackgroundImage dot_pattern.png
-    moveBackgroundImage inset_circle_tiny.png
-    moveBackgroundImage slider_run_base_horiz.png
-    moveBackgroundImage split_none_inset.png
-    moveBackgroundImage inset_round_hilight.png
-    moveBackgroundImage home_hilight.png
-    moveBackgroundImage slot_horiz_top.png
-    moveBackgroundImage holes_tiny_horiz.png
+    moveBackgroundImage bevel_out.png
+    moveBackgroundImage bg_radgrad.png
     moveBackgroundImage bub_base_b0.png
     moveBackgroundImage bub_base_b1.png
     moveBackgroundImage bub_base_b2.png
@@ -329,13 +284,52 @@ moveAllBackgroundImages(){
     moveBackgroundImage bub_base_br.png
     moveBackgroundImage bub_base_tl.png
     moveBackgroundImage bub_base_tr.png
+    moveBackgroundImage button_clicked.png
+    moveBackgroundImage button_normal.png
+    moveBackgroundImage darken_square.png
     moveBackgroundImage day_left_normal.png
     moveBackgroundImage day_middle_normal.png
     moveBackgroundImage day_right_normal.png
     moveBackgroundImage day_single_normal.png
+    moveBackgroundImage dot_pattern.png
+    moveBackgroundImage hole_pixel.png
+    moveBackgroundImage holes_horiz.png
+    moveBackgroundImage holes_tiny_horiz.png
+    moveBackgroundImage holes_tiny_vert.png
+    moveBackgroundImage holes_vert.png
+    moveBackgroundImage hole_tiny.png
+    moveBackgroundImage home_hilight.png
+    moveBackgroundImage home_inset.png
     moveBackgroundImage inset_bar_horiz_base.png
     moveBackgroundImage inset_bar_vert_base.png
+    moveBackgroundImage inset_circle_tiny.png
+    moveBackgroundImage inset_round_hilight.png
+    moveBackgroundImage inset_round_shading.png
+    moveBackgroundImage inset_round_shadow.png
+    moveBackgroundImage kbd_hilight.png
+    moveBackgroundImage kbd_inset.png
+    moveBackgroundImage led_square_shading.png
+    moveBackgroundImage mini_box_bevel_shadow.png
     moveBackgroundImage rounded_square.png
+    moveBackgroundImage runner_horiz.png
+    moveBackgroundImage runner_vert.png
+    moveBackgroundImage separator_horiz.png
+    moveBackgroundImage slider_run_base_horiz.png
+    moveBackgroundImage slider_run_base_light_horiz.png
+    moveBackgroundImage slider_run_base_light_vert.png
+    moveBackgroundImage slider_run_base_vert.png
+    moveBackgroundImage slider_run_bevel_horiz.png
+    moveBackgroundImage slider_run_bevel_vert.png
+    moveBackgroundImage slot_horiz_bottom.png
+    moveBackgroundImage slot_horiz_top.png
+    moveBackgroundImage spanner_hilight.png
+    moveBackgroundImage spanner_inset.png
+    moveBackgroundImage split_h_hilight.png
+    moveBackgroundImage split_h_inset.png
+    moveBackgroundImage split_none_hilight.png
+    moveBackgroundImage split_none_inset.png
+    moveBackgroundImage split_v_hilight.png
+    moveBackgroundImage split_v_inset.png
     moveBackgroundImage tooltip-base.png
     moveBackgroundImage tooltip-corner-bottom-left-tip.png
     moveBackgroundImage tooltip-corner-bottom-right-tip.png
@@ -345,6 +339,12 @@ moveAllBackgroundImages(){
     moveBackgroundImage tooltip-edge-left-tip.png
     moveBackgroundImage tooltip-edge-right-tip.png
     moveBackgroundImage tooltip-edge-top-tip.png
+    moveBackgroundImage vgrad_med_curved.png
+    moveBackgroundImage vgrad_med_darker.png
+    moveBackgroundImage vgrad_med_dark.png
+    moveBackgroundImage vgrad_med_lighter.png
+    moveBackgroundImage vgrad_med.png
+    moveBackgroundImage vgrad_tall.png
 }
 
 moveAllShadowImages(){
