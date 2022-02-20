f147df5619b7776bafca2e4b4aa64713fd283353
Simon Lees <simon@simotek.net>
2022-02-20 17:34:27 +1030

    Add a release script - not tested

---
+++ release-darkmod.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION=20220219.1.26
+
+branches=("Dark" "Ice" "Neonz" "openSUSE-e-Ice" "openSUSE-e-Neon" "openSUSE-e-OliveLeaf")
+
+rm -r artifacts
+rm -r build
+
+git checkout master
+git tag -a -m "darkmod-release $VERSION" "$VERSION"
+
+for b in ${branches[@]}; do
+  echo "Processing $b"
+  git checkout $b
+  git merge master
+  git tag -a -m "darkmod-release $VERSION" "$VERSION-$b"
+  ./package-darkmod.sh
+  ./build-darkmod.sh
+  git push
+done
+
+git checkout master
+git push
